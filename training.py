caa82274a1f7b710ae263632a1257ec7b0ec5f43
Andy Jones <andyjones.ed@gmail.com>
2015-07-09 09:03:38 +0100

    Updates logging

--- training.py
+++ training.py
@@ -4,9 +4,33 @@ Created on Thu Jul  9 08:52:01 2015
 
 @author: andy
 """
+import logging
+import interactive_console_options
 
 from models import *
 from batch_generation import make_batch_generator
+        
+def _make_train_model_coroutine(model, batch_gen, test_interval):
+    batches_seen = 0
+    last_epoch = 0
+    while True:
+        epoch, batch, X_batch, Y_batch = next(batch_gen)
+        
+        epoch_has_changed = (epoch != last_epoch)
+        if epoch_has_changed:
+            last_epoch = epoch
+            batches_seen = 0
+        
+        yield_for_testing = (batches_seen % test_interval == 0)
+        if yield_for_testing:
+            yield epoch
+            
+        loss = model.train_on_batch(X_batch, Y_batch)[()]
+        
+        if batches_seen % 10 == 0: 
+            logging.info('Epoch {}, batch {}, training loss {:4f}'.format(epoch, batch, loss))        
+        
+        batches_seen += 1        
 
 def _make_test_model_coroutine(model, batch_gen):
     total_loss = 0.
@@ -15,33 +39,18 @@ def _make_test_model_coroutine(model, batch_gen):
     while True:
         epoch, batch, X_batch, Y_batch = next(batch_gen)
         
-        loss = model.test_on_batch(X_batch, Y_batch)[()]
-        total_loss += loss
-        total_seen += len(X_batch)        
-        
-        if epoch != last_epoch:
-            print('Testing loss {:4f}'.format(epoch, loss))   
+        epoch_has_changed = (epoch != last_epoch)
+        if epoch_has_changed:
+            logging.info('Testing loss {:4f}'.format(total_loss/total_seen))   
             yield epoch
             
             last_epoch = epoch
             total_loss = 0.
-            total_seen = 0
+            total_seen = 0        
         
-def _make_train_model_coroutine(model, batch_gen, test_interval):
-    batches_seen = 0
-    last_epoch = 0
-    while True:
-        epoch, batch, X_batch, Y_batch = next(batch_gen)
-        
-        if epoch != last_epoch:
-            last_epoch = epoch
-            batches_seen = 0
-        if batches_seen % test_interval == 0:
-            yield epoch
-            
-        loss = model.train_on_batch(X_batch, Y_batch)[()]
-        print('Epoch {}, batch {}, training loss {:4f}'.format(epoch, batch, loss))        
-        batches_seen += 1        
+        loss = model.test_on_batch(X_batch, Y_batch)[()]
+        total_loss += loss
+        total_seen += len(X_batch)           
         
 def train_and_test_model(model, text, total_epochs=100, test_interval=1000):
     train_batch_gen, _ = make_batch_generator(text, active_range=(0., 0.95))  
