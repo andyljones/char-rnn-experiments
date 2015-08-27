00f6c77f6cb808aa86bcb6989dfe7d3e92a7a3c1
Andrew Jones <andyjones.ed@gmail.com>
2015-08-27 19:24:49 +0100

    Finishes the rewrite of the GRU into a single graph

--- gru.lua
+++ gru.lua
@@ -29,18 +29,22 @@ function build(n_timesteps, n_symbols, n_neurons)
   local input = nn.Identity()()
   local inputs = {input:split(n_timesteps)}
 
+  local modules = {}
   local outputs = {}
 
   local initial_state = nn.Identity()()
   local hidden_state = initial_state
   for i = 1, n_timesteps do
-    hidden_state = build_cell(inputs[i], hidden_state, n_symbols, n_neurons)
-    outputs[i] = nn.LogSoftMax()(nn.Linear(n_neurons, n_symbols)(hidden_state))
+    local new_hidden_state = build_cell(inputs[i], hidden_state, n_symbols, n_neurons)
+    local output = nn.LogSoftMax()(nn.Linear(n_neurons, n_symbols)(new_hidden_state))
+    outputs[i] = nn.Reshape(-1, 1, n_symbols, false)(output)
+    -- modules[i] = nn.gModule({inputs[i], hidden_state}, {outputs[i], new_hidden_state})
+    hidden_state = new_hidden_state
   end
 
-  return nn.gModule({input, initial_state}, outputs)
-end
+  local output = nn.JoinTable(2)(outputs)
 
-build(10, 20, 20)
+  return nn.gModule({input, initial_state}, {output, hidden_state})
+end
 
 return {build=build}
--- training.lua
+++ training.lua
@@ -4,20 +4,30 @@ local encoding = require 'encoding'
 local torch = require 'torch'
 local table = require 'std.table'
 
+function decode(alphabet, batch)
+  local results = {}
+  for i = 1, batch:size(1) do
+    results[i] = encoding.one_hot_to_chars(alphabet, batch[i])
+  end
+
+  return results
+end
+
 function reshape(batch)
-  local sequences = batch:transpose(1, 2):split(1, batch:size(1))
+  local sequences = batch:transpose(1, 2):split(1, 1)
   for i = 1, #sequences do
-    sequences[i] = sequences[i]:view(batch:size(1), batch:size(3))
+    sequences[i] = sequences[i]:clone():view(batch:size(1), batch:size(3))
   end
   return sequences
 end
 
-local n_neurons, n_timesteps, n_samples = 20, 10, 1
+local n_neurons, n_timesteps, n_samples = 20, 10, 2
 
 local text = batcher.load_text()
 local alphabet, batch_iterator = batcher.make_batch_iterators(text, torch.Tensor{1}, n_timesteps, n_samples)
 
 local model = gru.build(n_timesteps, table.size(alphabet), n_neurons)
-local initial_state = torch.zeros(1, n_neurons)
+local initial_state = torch.zeros(n_samples, n_neurons)
 local batch = reshape(batch_iterator[1]())
-model:forward({batch, initial_state})
+local output, final_state = unpack(model:forward({batch, initial_state}))
+for _, v in pairs(decode(alphabet, output)) do print(v) end
