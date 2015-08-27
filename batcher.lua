f839800daf2c21db1013644bc86160a9ccb4cff2
Andrew Jones <andyjones.ed@gmail.com>
2015-08-27 10:19:38 +0100

    Adds batch iterators

--- batcher.lua
+++ batcher.lua
@@ -72,6 +72,51 @@ function make_chunk_iterators(text, split_fractions, chunk_size)
   return alphabet, iterators
 end
 
+function stack(tensors)
+  local shape = torch.totable(tensors[1]:size())
+  table.insert(shape, 1, #tensors)
+
+  local result = torch.Tensor(torch.LongStorage(shape))
+  for i = 1, #tensors do
+    result[i] = tensors[i]
+  end
+
+  return result
+end
+
+function make_batch_iterator(chunk_iterator, batch_size)
+  function co()
+    local batch = {}
+    while true do
+      local chunk = chunk_iterator()
+      if chunk then
+        batch[#batch + 1] = chunk
+      else
+        break
+      end
+
+      if #batch == batch_size then
+        coroutine.yield(stack(batch))
+        batch = {}
+      end
+    end
+  end
+
+  return coroutine.wrap(co)
+end
+
+function make_batch_iterators(text, split_fractions, chunk_size, batch_size)
+  local alphabet, chunk_iterators = make_chunk_iterators(text, split_fractions, chunk_size)
+
+  local batch_iterators = {}
+  for _, chunk_iterator in pairs(chunk_iterators) do
+    batch_iterators[#batch_iterators + 1] =  make_batch_iterator(chunk_iterator, batch_size)
+  end
+
+  return alphabet, batch_iterators
+end
+
+
 -- TESTS --
 local luaunit = require('luaunit')
 
@@ -125,5 +170,20 @@ function test_make_chunk_iterators()
     luaunit.assertEquals(count, expected_lengths[i])
   end
 end
+
+function test_make_batch_iterators()
+  local text = 'abbcbaccabbcbacc'
+  local fractions = torch.Tensor{0.25, 0.75}
+
+  local _, iterators = make_batch_iterators(text, fractions, 2, 2)
+
+  local expected_lengths = {1, 3}
+  for i = 1, #expected_lengths do
+    local count = 0
+    while iterators[i]() do count = count + 1 end
+    luaunit.assertEquals(count, expected_lengths[i])
+  end
+end
+
 
 luaunit.LuaUnit.run()
