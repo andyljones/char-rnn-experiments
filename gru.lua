4e7500f0accc7262ccb577ab6c523f257a0e9c48
Andrew Jones <andyjones.ed@gmail.com>
2015-08-27 19:51:34 +0100

    Enforces parameter sharing

--- gru.lua
+++ gru.lua
@@ -3,20 +3,20 @@ local nn = require 'nn'
 require 'nngraph'
 require 'graph'
 
-function compose_inputs(input_size, n_neurons, input, prev_hidden)
-  local input_to_hidden = nn.Linear(input_size, n_neurons)(input)
-  local hidden_to_hidden = nn.Linear(n_neurons, n_neurons)(prev_hidden)
+function compose_inputs(input_size, n_neurons, input, prev_hidden, name)
+  local input_to_hidden = nn.Linear(input_size, n_neurons)(input):annotate{name=name..'-i2h'}
+  local hidden_to_hidden = nn.Linear(n_neurons, n_neurons)(prev_hidden):annotate{name=name..'-i2h'}
   return nn.CAddTable()({input_to_hidden, hidden_to_hidden})
 end
 
 function build_cell(input, prev_hidden, input_size, n_neurons)
-  local reset_gate = nn.Sigmoid()(compose_inputs(input_size, n_neurons, input, prev_hidden))
+  local reset_gate = nn.Sigmoid()(compose_inputs(input_size, n_neurons, input, prev_hidden, 'reset'))
   local reset_hidden = nn.CMulTable()({reset_gate, prev_hidden})
-  local transformed_reset_hidden = nn.Linear(n_neurons, n_neurons)(reset_hidden)
-  local transformed_input = nn.Linear(input_size, n_neurons)(input)
+  local transformed_reset_hidden = nn.Linear(n_neurons, n_neurons)(reset_hidden):annotate{name='trh'}
+  local transformed_input = nn.Linear(input_size, n_neurons)(input):annotate{name='ti'}
   local candidate_hidden = nn.Tanh()(nn.CAddTable()({transformed_input, transformed_reset_hidden}))
 
-  local update_gate = nn.Sigmoid()(compose_inputs(input_size, n_neurons, input, prev_hidden))
+  local update_gate = nn.Sigmoid()(compose_inputs(input_size, n_neurons, input, prev_hidden, 'update'))
   local update_candidate = nn.CMulTable()({update_gate, candidate_hidden})
   local update_compliment = nn.AddConstant(1)(nn.MulConstant(-1)(update_gate))
   local update_compliment_prev = nn.CMulTable()({update_compliment, prev_hidden})
@@ -25,11 +25,26 @@ function build_cell(input, prev_hidden, input_size, n_neurons)
   return next_hidden
 end
 
+function share_matched_names(module)
+  local nodes = module.fg.nodes
+  local originals = {}
+  for i = 1, #nodes do
+    local node = nodes[i]
+    local module = node.data.module
+    local name = node.data.annotations.name
+    if name and originals[name] then
+      module:share(originals[name])
+    elseif name then
+      originals[name] = module
+    end
+  end
+end
+
+
 function build(n_timesteps, n_symbols, n_neurons)
   local input = nn.Identity()()
   local inputs = {input:split(n_timesteps)}
 
-  local modules = {}
   local outputs = {}
 
   local initial_state = nn.Identity()()
@@ -38,13 +53,14 @@ function build(n_timesteps, n_symbols, n_neurons)
     local new_hidden_state = build_cell(inputs[i], hidden_state, n_symbols, n_neurons)
     local output = nn.LogSoftMax()(nn.Linear(n_neurons, n_symbols)(new_hidden_state))
     outputs[i] = nn.Reshape(-1, 1, n_symbols, false)(output)
-    -- modules[i] = nn.gModule({inputs[i], hidden_state}, {outputs[i], new_hidden_state})
     hidden_state = new_hidden_state
   end
 
   local output = nn.JoinTable(2)(outputs)
 
-  return nn.gModule({input, initial_state}, {output, hidden_state})
+  module = nn.gModule({input, initial_state}, {output, hidden_state})
+  share_matched_names(module)
+  return module
 end
 
 return {build=build}
