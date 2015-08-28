609be246e4624b3b81665e371308a4e340b919c9
Andrew Jones <andyjones.ed@gmail.com>
2015-08-28 08:26:16 +0100

    Minor changes in an attempt to get training working. No luck yet.

--- training.lua
+++ training.lua
@@ -3,6 +3,7 @@ local gru = require 'gru'
 local encoding = require 'encoding'
 local torch = require 'torch'
 local table = require 'std.table'
+-- local model_utils = require 'model_utils'
 require 'optim'
 
 function decode(alphabet, batch)
@@ -60,20 +61,21 @@ function initialize(model)
   params:uniform(-0.08, 0.08)
 end
 
-local n_neurons, n_timesteps, n_samples = 128, 50, 50
-local grad_clip = 5
-local optim_state = {learningRate = 2e-3, alpha=0.95}
-
-local text = batcher.load_text()
-local alphabet, batch_iterators = batcher.make_batch_iterators(text, torch.Tensor{1}, n_timesteps, n_samples)
-
-local training_iterator = batch_iterators[1]
-local model = gru.build(n_timesteps, table.size(alphabet), n_neurons)
-initialize(model)
-
-local feval = make_feval(model, training_iterator, n_neurons, grad_clip)
-local params, _ = model:getParameters()
-for i = 1, 100 do
-  local _, loss = optim.rmsprop(feval, params, optim_state)
-  print(i, loss[1])
-end
+-- local n_neurons, n_timesteps, n_samples = 128, 50, 50
+-- local grad_clip = 5
+-- local optim_state = {learningRate=2e-3, alpha=0.95}
+--
+-- local text = batcher.load_text()
+-- local alphabet, batch_iterators = batcher.make_batch_iterators(text, torch.Tensor{1}, n_timesteps, n_samples)
+--
+-- local training_iterator = batch_iterators[1]
+-- local model = gru.build(n_timesteps, table.size(alphabet), n_neurons)
+-- initialize(model)
+--
+-- local feval = make_feval(model, training_iterator, n_neurons, grad_clip)
+-- local params, _ = model:getParameters()
+--
+-- for i = 1, 5 do
+--   local _, loss = optim.rmsprop(feval, params, optim_state)
+--   print(i, loss[1])
+-- end
