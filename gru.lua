ff06450327d37c3c61ffe3cd0151e09682d8d7ee
Andrew Jones <andyjones.ed@gmail.com>
2015-08-31 00:06:47 +0100

    Adds support for single-layer GRUs, but something seems to have broken

--- gru.lua
+++ gru.lua
@@ -24,22 +24,29 @@ end
 function M.build(n_symbols, n_neurons, n_layers)
   local input = nn.Identity()()
   local prev_hidden = nn.Identity()()
-  local prev_hiddens = {nn.SplitTable(1, 2)(prev_hidden):split(n_layers)}
+  local prev_hiddens
 
-  prev_hiddens[1] = nn.Reshape(n_neurons, true)(prev_hiddens[1])
+  if n_layers > 1 then
+    prev_hiddens = {nn.SplitTable(1, 2)(prev_hidden):split(n_layers)}
+  else
+    prev_hiddens = {nn.Reshape(n_neurons, true)(prev_hidden)}
+  end
+
   local next_hiddens = {M.build_cell(input, prev_hiddens[1], n_symbols, n_neurons, tostring(1))}
   for i = 2, n_layers do
-    prev_hiddens[i] = nn.Reshape(n_neurons, true)(prev_hiddens[i])
     next_hiddens[i] = M.build_cell(next_hiddens[i-1], prev_hiddens[i], n_neurons, n_neurons, tostring(i))
   end
 
   local output = nn.Linear(n_neurons, n_symbols)(next_hiddens[n_layers]):annotate{name='out'}
 
-  for i = 1, n_layers do
-    next_hiddens[i] = nn.Reshape(1, n_neurons, true)(next_hiddens[i])
-  end
-  local next_hidden = nn.JoinTable(1, 2)(next_hiddens)
+  for i = 1, n_layers do next_hiddens[i] = nn.Reshape(1, n_neurons, true)(next_hiddens[i]) end
 
+  local next_hidden
+  if n_layers > 1 then
+    next_hidden = nn.JoinTable(1, 2)(next_hiddens)
+  else
+    next_hidden = next_hiddens[1]
+  end
 
   local module = nn.gModule({input, prev_hidden}, {output, next_hidden})
 
